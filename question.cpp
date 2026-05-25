9c34d1e88bffa83b8e8427cddfd1d04b8c3fb22b
Monil gautam <87960001+Aw1613@users.noreply.github.com>
2026-05-25 08:32:32 +0530

    Bubble sort logic

--- question.cpp
+++ question.cpp
@@ -1,7 +1,21 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout<<"hello";
-    return 0;
+// Bubble sort
+
+int main() {
+	int arr[] = {24,13,52,46,9,20};
+	
+	for(int i = 6-1; i >= 0; i--){
+	    for(int k = 0; k <= i; k++){
+	        if(arr[k] > arr[k+1]){
+	            swap(arr[k], arr[k+1]);
+	        }
+	    }
+	}
+	for(int i = 0; i<= 5; i++){
+	    cout<<arr[i]<<" ";
+	}
+	
+    
 }
