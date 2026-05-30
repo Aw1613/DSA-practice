e106adbc27bf45e804ee5249879ab5bc1435a8b7
Monil gautam <87960001+Aw1613@users.noreply.github.com>
2026-05-30 13:08:14 +0530

    merge sort algo

---
+++ algo_inventory.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+void mrg(vector<int> &arr, int low , int mid , int high){
+    int left = low;
+    int right = mid+1;
+    vector<int> temp;
+    
+    while((left <= mid) && (right <= high)){
+        if(arr[left] <= arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    
+    while (left <= mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while (right <= high){
+        temp.push_back(arr[right]);
+        right++;
+    }
+    
+    for(int i = low; i <= high; i++){
+        arr[i] = temp[i - low];
+    }
+}
+
+void ms(vector<int> &arr, int low, int high){
+    if(low == high) return;
+    int mid = (low + high) / 2;
+    ms(arr , low, mid);
+    ms(arr , mid+1 , high);
+    mrg(arr , low , mid , high);
+}
+
+int main() {
+	vector<int> ary = {3,2,4,1,3,8,2};
+	ms(ary , 0 , 6);
+	for(int i = 0; i < ary.size(); i++){
+	    cout<<ary[i]<<" ";
+	}
+
+}
