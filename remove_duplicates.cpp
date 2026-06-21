700fbb7b097ea9820b11f6e312207a185925d37c
Monil gautam <87960001+Aw1613@users.noreply.github.com>
2026-06-21 11:27:07 +0530

    attempt 2 [r]

--- remove_duplicates.cpp
+++ remove_duplicates.cpp
@@ -1,17 +1,39 @@
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         int temp = -101;
+//         for(auto it = nums.begin(); it != nums.end(); it++){
+//             if(*it != temp){
+//                 temp = *it;
+//             }
+//             else if(*it == temp){
+//                 // TC will increase due to shifiting of elements 
+//                 nums.erase(it);
+//                 it--;
+//             }
+//         }
+//         return nums.size();
+//     }
+// };
+
+
+// new attempt 
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int temp = -101;
-        for(auto it = nums.begin(); it != nums.end(); it++){
-            if(*it != temp){
-                temp = *it;
+        int i = 0;
+        int j = 1;
+        int n = nums.size();
+        while(j < n && i < n){
+            if(nums[i] == nums[j]){
+                j++;
             }
-            else if(*it == temp){
-                // TC will increase due to shifiting of elements 
-                nums.erase(it);
-                it--;
+            else if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+                j++;
             }
         }
-        return nums.size();
+        return i+1;
     }
 };
