6d4c46587891fafb5a6b2eaaab0030bcf5c72ca6
Monil gautam <87960001+Aw1613@users.noreply.github.com>
2026-06-21 11:44:22 +0530

    attempt 2 [r]

--- move_0.cpp
+++ move_0.cpp
@@ -1,14 +1,30 @@
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         auto ptr = nums.begin();
+//         for(auto it = nums.begin(); it != nums.end(); it++){
+//             if(*ptr != 0 ){
+//                 ptr++;
+//             }
+//             else if(*ptr == 0 && *it != 0){
+//                 swap(*ptr,*it);
+//                 ptr++;
+//             }
+//         }
+//     }
+// };
+
+// 2nd attempt 
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        auto ptr = nums.begin();
-        for(auto it = nums.begin(); it != nums.end(); it++){
-            if(*ptr != 0 ){
-                ptr++;
-            }
-            else if(*ptr == 0 && *it != 0){
-                swap(*ptr,*it);
-                ptr++;
+        int i = 0;
+        for(int j = 1; j < nums.size(); j++){
+            if(nums[i] != 0) i++;
+            else if( nums[i] == 0 && nums[j] != 0){
+                swap(nums[i], nums[j]);
+                i++;
+                
             }
         }
     }
