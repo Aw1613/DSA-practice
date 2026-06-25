3e7afdd6cdfa7e5d2751657cf22fd4a2cb07d534
Monil gautam <87960001+Aw1613@users.noreply.github.com>
2026-06-25 23:44:39 +0530

    Two sum brute way

---
+++ array/two_sum.cpp
@@ -0,0 +1,22 @@
+// brute force approach 
+// TLE in big test case
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> sol;
+        for(int i = 0; i < nums.size()-1; i++){
+            cout<<i<<endl;
+            for(int j = nums.size()-1; j  0; j--){
+                cout<<"j : "<<j<<endl;
+                if(nums[i] + nums[j] == target){
+                    sol.push_back(i);
+                    sol.push_back(j);
+                    return sol;
+                }
+            }
+        }
+        return sol;
+    }
+};
+
