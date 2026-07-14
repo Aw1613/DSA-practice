4fe5256a25a9b7436023cb35020e6950ac0f0710
Monil gautam <87960001+Aw1613@users.noreply.github.com>
2026-07-14 18:37:49 +0530

    Boyer-Moore Voting Algo solution

--- array/majority_element.cpp
+++ array/majority_element.cpp
@@ -1,23 +1,47 @@
 // brute force solution with SC = O(N)
 
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         unordered_map<int,int> check_map;
+//         int target = nums.size();
+//         for(int i = 0; i < nums.size(); i++){
+//             auto it = check_map.find(nums[i]);
+//             if(it != check_map.end()){
+//                 check_map[nums[i]]++;
+//                 if(it->second > target/2){
+//                     return it->first;
+//                 }
+//             }
+//             else{
+//                 cout<<i<<endl;
+//                 check_map[nums[i]] = 1;
+//             }
+//         }
+//         return nums[0];
+//     }
+// };
+
+
+// Boyer-Moore Voting Algorithm TC = O(N) SC = O(1)
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> check_map;
-        int target = nums.size();
+        int el;
+        int cnt = 0;
         for(int i = 0; i < nums.size(); i++){
-            auto it = check_map.find(nums[i]);
-            if(it != check_map.end()){
-                check_map[nums[i]]++;
-                if(it->second > target/2){
-                    return it->first;
-                }
+            if(cnt == 0){
+                el = nums[i];
+                cnt = 1;
             }
-            else{
-                cout<<i<<endl;
-                check_map[nums[i]] = 1;
+            else if(nums[i] != el){
+                cnt--;
+            }
+            else if(nums[i] == el){
+                cnt++;
             }
         }
-        return nums[0];
+        return el;
     }
 };
