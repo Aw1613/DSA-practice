f803749f7a229ec4abc21892d00f321288ba6a9c
Monil gautam <87960001+Aw1613@users.noreply.github.com>
2026-07-19 10:41:46 +0530

    Optimal solution help

--- array/next_permutaiton.cpp
+++ array/next_permutaiton.cpp
@@ -23,27 +23,55 @@
 
 // attempt 2 RTE
 
+// class Solution {
+// public:
+//     void nextPermutation(vector<int>& nums) {
+//         int l = nums.size();
+//         bool yes = 0;
+//         for(int i = l-2; l >= 0; i--){
+//             if(nums[i] < nums[i+1]){
+//                 yes = 1;
+//                 for(int k = l-1; k > i; k--){
+//                     if(nums[k] > nums[i]){
+//                         swap(nums[k],nums[i]);
+//                         break;
+//                     }
+//                 }
+//                 sort(nums.begin() + i + 1, nums.end());
+//                 return ;
+//             }
+//         }
+//         if(yes == 0){
+//             reverse(nums.begin(), nums.end());
+//             return;
+//         }
+//     }
+// };
+
+// Optimal solution
+
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int l = nums.size();
-        bool yes = 0;
-        for(int i = l-2; l >= 0; i--){
+        int ind = -1;
+        for(int i = l-2; i >= 0; i--){
             if(nums[i] < nums[i+1]){
-                yes = 1;
-                for(int k = l-1; k > i; k--){
-                    if(nums[k] > nums[i]){
-                        swap(nums[k],nums[i]);
-                        break;
-                    }
-                }
-                sort(nums.begin() + i + 1, nums.end());
-                return ;
+                ind = i;
+                break;
             }
         }
-        if(yes == 0){
-            reverse(nums.begin(), nums.end());
+        if(ind == -1){
+            reverse(nums.begin(),nums.end());
             return;
         }
+        for(int i = l-1; i > ind; i--){
+            if(nums[i] > nums[ind]){
+                swap(nums[ind],nums[i]);
+                break;
+            }
+        }
+        reverse(nums.begin() + ind + 1, nums.end());
+        return ;
     }
 };
