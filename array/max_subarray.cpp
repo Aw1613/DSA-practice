ebd16dda75cac2efdbf677b3f8d7150881a8032e
Monil gautam <87960001+Aw1613@users.noreply.github.com>
2026-07-28 07:12:56 +0530

    sol is optimal but code style not good

--- array/max_subarray.cpp
+++ array/max_subarray.cpp
@@ -1,20 +1,41 @@
 // brute force solution with TLE error in huge case 
 
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+//         int total = 0;
+//         // auto it = min_element(nums.begin(), nums.end());
+//         int max = 0;
+//         for(int i = 0; i < nums.size(); i++){
+//             for(int j = i; j < nums.size(); j++){
+//                 total = total + nums[j];
+//                 if(total > max){
+//                     max = total;
+//                 }
+//             }
+//             total = 0;
+//         }
+//         return max;
+//     }
+// };
+
+
+// ATP 2 , using kadane's algo 
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int total = 0;
-        // auto it = min_element(nums.begin(), nums.end());
-        int max = 0;
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        int temp = 0;
         for(int i = 0; i < nums.size(); i++){
-            for(int j = i; j < nums.size(); j++){
-                total = total + nums[j];
-                if(total > max){
-                    max = total;
-                }
+            temp = temp + nums[i];
+            if(temp < 0){
+                sum = max(sum,temp);
+                temp = 0;
             }
-            total = 0;
+            else if(temp >= 0){
+                sum = max(sum,temp);
+            }
         }
-        return max;
+        return sum;
     }
 };
