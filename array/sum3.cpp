ed3ced2a56f80577b8f4a88e0a681c008c1787f3
Monil gautam <87960001+Aw1613@users.noreply.github.com>
2026-08-01 13:03:09 +0530

    Optimal solution

--- array/sum3.cpp
+++ array/sum3.cpp
@@ -47,26 +47,68 @@
 
 
 // stirver better solution but TLE
+// class Solution {
+// public:
+//     vector<vector<int>> threeSum(vector<int>& nums) {
+//         set<vector<int>> sol;
+//         int n = nums.size();
+
+//         for(int i = 0; i <n; i++){
+//             set<int> hashset;
+//             for(int j = i+1; j < n; j++){
+//                 int search = -1*(nums[i] + nums[j]);
+//                 if (hashset.find(search) != hashset.end()){
+//                     vector<int> temp = {nums[i] , nums[j], search};
+//                     sort(temp.begin(), temp.end());
+//                     sol.insert(temp);
+//                 }
+//                 hashset.insert(nums[j]);
+//             }
+//         }
+
+//         vector<vector<int>> ans(sol.begin(), sol.end());
+//         return ans;
+//     }
+// };
+
+// Optimal solution 
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        set<vector<int>> sol;
+        sort(nums.begin(), nums.end());
         int n = nums.size();
+        vector<vector<int>> sol;
 
-        for(int i = 0; i <n; i++){
-            set<int> hashset;
-            for(int j = i+1; j < n; j++){
-                int search = -1*(nums[i] + nums[j]);
-                if (hashset.find(search) != hashset.end()){
-                    vector<int> temp = {nums[i] , nums[j], search};
-                    sort(temp.begin(), temp.end());
-                    sol.insert(temp);
-                }
-                hashset.insert(nums[j]);
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
             }
-        }
+            int j = i+1;
+            int k = n-1;
+            int sum;
+            while(j < k){
+                sum = nums[i] + nums[j] + nums[k];
+                if(sum == 0){
+                    sol.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    while(j < k && nums[j] == nums[j-1]) j++;
+                    while(j < k && nums[k] == nums[k+1]) k--;
+                }
+                else if(sum > 0){
+                    k--;
+                    // while(j < k && nums[k] == nums[k+1]) k--;
+                    // we don't need this because if the element would be the same then automatically same condition
+                    //will come and this k-- takes place
+                }
+                else if(sum < 0){
+                    j++;
+                    // while(j < k && nums[j] == nums[j-1]) j++;
+                }
 
-        vector<vector<int>> ans(sol.begin(), sol.end());
-        return ans;
+            }
+
+        }
+        return sol;
     }
 };
