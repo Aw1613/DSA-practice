a3c07323b15da1f06ed5cb0ff8bf0ecf610004b6
Monil gautam <87960001+Aw1613@users.noreply.github.com>
2026-08-03 23:39:00 +0530

    ATP 3

--- array/max_subarray.cpp
+++ array/max_subarray.cpp
@@ -21,21 +21,42 @@
 
 
 // ATP 2 , using kadane's algo 
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+//         int sum = accumulate(nums.begin(),nums.end(),0);
+//         int temp = 0;
+//         for(int i = 0; i < nums.size(); i++){
+//             temp = temp + nums[i];
+//             if(temp < 0){
+//                 sum = max(sum,temp);
+//                 temp = 0;
+//             }
+//             else if(temp >= 0){
+//                 sum = max(sum,temp);
+//             }
+//         }
+//         return sum;
+//     }
+// };
+
+// negative case couldn't handel properly 
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum = accumulate(nums.begin(),nums.end(),0);
+        auto it = min_element(nums.begin(),nums.end());
+        int sum = *it;
+        cout<<sum<<endl;
         int temp = 0;
         for(int i = 0; i < nums.size(); i++){
             temp = temp + nums[i];
+            sum = max(temp,sum);
             if(temp < 0){
-                sum = max(sum,temp);
                 temp = 0;
             }
-            else if(temp >= 0){
-                sum = max(sum,temp);
-            }
         }
+        temp = *min_element(nums.begin(),nums.end());
+        sum = max(sum,temp);
         return sum;
     }
 };
