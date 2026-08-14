292a07d1374c8707f63e5bb554ce812291ae3a20
Monil gautam <87960001+Aw1613@users.noreply.github.com>
2026-08-15 00:40:21 +0530

    Optimal sol

--- array/majority_element_2.cpp
+++ array/majority_element_2.cpp
@@ -54,21 +54,69 @@
 
 // ATP 2
 // edge case couldn't handel properly 
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//         unordered_map<int,int> mpp;
+//         vector<int> sol;
+//         int t = floor((nums.size())/3);
+//         for(int i = 0; i < nums.size() ; i++){
+//             // if(i == 0) mpp[nums[i]]++;
+//             if(mpp[nums[i]] <= t){
+//                 mpp[nums[i]]++;
+//             }
+//             else if(mpp[nums[i]] > t){
+//                 sol.push_back(nums[i]);
+//             }
+//         }
+//         return sol;
+//     }
+// };
+
+
+// Optimal solution 
+
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mpp;
+        int el1 = INT_MIN;
+        int el2 = INT_MIN;
+        int cnt1 = 0, cnt2 = 0;
         vector<int> sol;
-        int t = floor((nums.size())/3);
-        for(int i = 0; i < nums.size() ; i++){
-            // if(i == 0) mpp[nums[i]]++;
-            if(mpp[nums[i]] <= t){
-                mpp[nums[i]]++;
+        for(int i = 0; i < nums.size(); i++){
+            if(cnt1 == 0 && el2 != nums[i]){
+                el1 = nums[i];
+                cnt1 = 1;
             }
-            else if(mpp[nums[i]] > t){
-                sol.push_back(nums[i]);
+            else if(cnt2 == 0 && el1 != nums[i]){
+                el2 = nums[i];
+                cnt2 = 1;
+            }
+            else if(el1 == nums[i]){
+                cnt1++;
+            }
+            else if(el2 == nums[i]){
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
+        cnt1 = 0;
+        cnt2 = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(el1 == nums[i]){
+                cnt1++;
+            }
+            else if(el2 == nums[i]){
+                cnt2++;
+            }
+        }
+        int req = (int)(nums.size()/3);
+        if(cnt1 > req) sol.push_back(el1);
+        if(cnt2 > req) sol.push_back(el2);
         return sol;
+
     }
 };
