6f187141a615f4e3429a3a3dd8f0c14e7f9b6da9
Monil gautam <87960001+Aw1613@users.noreply.github.com>
2026-08-18 11:03:56 +0530

    Failed better approach

--- array/merge_intervals.cpp
+++ array/merge_intervals.cpp
@@ -1,30 +1,83 @@
 // failed BF solution 
 
+// class Solution {
+// public:
+//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+//         vector<vector<int>> sol;
+//         for(int i = 0; i < intervals.size() - 1; i++){
+//             if(intervals[i][0] > intervals[i+1][0] && intervals[i+1][0] > intervals[i][1]){
+//                 if(intervals[i][1] > intervals[i+1][1]){
+//                     vector<int> tup = {intervals[i][0] , intervals[i][1]};
+//                     sol.push_back(tup);
+//                 }
+//                 else{
+//                     vector<int> tup = {intervals[i][0] , intervals[i+1][1]};
+//                     sol.push_back(tup);
+//                 }
+//             }
+//             else if(intervals[i+1][0] > intervals[i][0] && intervals[i][0] > intervals[i+1][1]){
+//                 if(intervals[i][1] > intervals[i+1][1]){
+//                     vector<int> tup = {intervals[i+1][0] , intervals[i][1]};
+//                     sol.push_back(tup);
+//                 }
+//                 else{
+//                     vector<int> tup = {intervals[i+1][0] , intervals[i+1][1]};
+//                 }
+//             }
+//         }
+//         return sol;
+//     }
+// };
+
+
+
+
+// better solution attemtp 2
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> sol;
-        for(int i = 0; i < intervals.size() - 1; i++){
-            if(intervals[i][0] > intervals[i+1][0] && intervals[i+1][0] > intervals[i][1]){
-                if(intervals[i][1] > intervals[i+1][1]){
-                    vector<int> tup = {intervals[i][0] , intervals[i][1]};
-                    sol.push_back(tup);
-                }
-                else{
-                    vector<int> tup = {intervals[i][0] , intervals[i+1][1]};
-                    sol.push_back(tup);
-                }
-            }
-            else if(intervals[i+1][0] > intervals[i][0] && intervals[i][0] > intervals[i+1][1]){
-                if(intervals[i][1] > intervals[i+1][1]){
-                    vector<int> tup = {intervals[i+1][0] , intervals[i][1]};
-                    sol.push_back(tup);
-                }
-                else{
-                    vector<int> tup = {intervals[i+1][0] , intervals[i+1][1]};
-                }
-            }
+        sort(intervals.begin(),intervals.end());
+        int num = -1;
+
+        int mini = min(intervals[0][0],intervals[0][1]);
+        int maxi = max(intervals[0][0],intervals[0][1]);
+        if(intervals.size() == 1){
+            vector<int> tup = {intervals[0][0],intervals[0][1]};
+            sol.push_back(tup);
         }
+        for(int i = 1; i < intervals.size(); i++){
+            int rl = min(intervals[i][0],intervals[i][1]);
+            int ru = max(intervals[i][0],intervals[i][1]);
+
+            if (mini <= rl && rl <= maxi){
+                if(maxi < ru){
+                    maxi = ru;
+                }
+                if(sol.empty() == true){
+                    vector<int> tup = {mini,maxi};
+                    sol.push_back(tup);
+                    num++;
+                }
+                else{
+                    sol[num][1] = maxi;
+                }
+            }
+            else {
+                if(i < intervals.size()-1){
+                    mini = min(intervals[i][0],intervals[i][1]);
+                    maxi = max(intervals[i][0],intervals[i][1]);
+                    vector<int> tup = {mini,maxi};
+                    sol.push_back(tup);
+                }
+                else{
+                    vector<int> tup = {intervals[i][0],intervals[i][1]};
+                    sol.push_back(tup);
+                }
+            }            
+
+        }
+        
         return sol;
     }
 };
