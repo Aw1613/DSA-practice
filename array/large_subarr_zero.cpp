c40cf0d6c77c90b325ad04fe6d706e1060241980
Monil gautam <87960001+Aw1613@users.noreply.github.com>
2026-08-27 11:26:11 +0530

    Optimal sol

--- array/large_subarr_zero.cpp
+++ array/large_subarr_zero.cpp
@@ -1,19 +1,43 @@
+// class Solution {
+//   public:
+//     int maxLen(vector<int>& arr) {
+//         // vector<int> triper;
+//         int l = arr.size();
+//         int maxi = 0;
+//         for(int i = 0; i < l; i++){
+//             vector<int> triper(arr.begin()+i, arr.end());
+//             int sum = std::accumulate(triper.begin(),triper.end(),0);
+//             while(sum != 0 && triper.size() >= 0){
+//                 int num = triper.back();
+//                 sum = sum - num;
+//                 triper.pop_back();
+//             }
+//             int len = triper.size();
+//             maxi = std::max(maxi,len);
+//         }
+//         return maxi;
+//     }
+// };
+
+
+// Optimal solution 
 class Solution {
   public:
     int maxLen(vector<int>& arr) {
-        // vector<int> triper;
         int l = arr.size();
+        int sum = 0;
         int maxi = 0;
+        map<int,int> mpp;
         for(int i = 0; i < l; i++){
-            vector<int> triper(arr.begin()+i, arr.end());
-            int sum = std::accumulate(triper.begin(),triper.end(),0);
-            while(sum != 0 && triper.size() >= 0){
-                int num = triper.back();
-                sum = sum - num;
-                triper.pop_back();
+            sum = sum + arr[i];
+            auto it = mpp.find(sum);
+            if(it != mpp.end()){
+                maxi = max(maxi,i-it->second);
             }
-            int len = triper.size();
-            maxi = std::max(maxi,len);
+            else{
+                mpp[sum] = i;
+            }
+            
         }
         return maxi;
     }
